694c300772b95f200ea64f49ee592f3f5df2741e
EvyKnops <knopsevy123@gmail.com>
2026-05-27 23:48:14 +0200

    added the scene and model now actually work in local host

--- room-portfolio/src/main.js
+++ room-portfolio/src/main.js
@@ -1,6 +1,8 @@
 import './style.scss'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
 const canvas = document.querySelector('#experience-canvas');
@@ -10,6 +12,31 @@ const sizes = {
 }
 
 const scene = new THREE.Scene();
+
+//loaderss
+const textureLoader = new THREE.TextureLoader();
+
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('/draco/');
+
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
+
+// weet niet zeker of ik deze nodig heb want heb geen aparte textures
+const modelPath = "/models/room_try2-v1.glb";
+gltfLoader.load(
+  modelPath,
+  (glb) => {
+    const model = glb.scene;
+    scene.add(model);
+    console.log("GLTF loaded:", model);
+  },
+  undefined,
+  (error) => {
+    console.error("GLTF load error:", error);
+  }
+);
+
 const camera = new THREE.PerspectiveCamera( 
   75,
   sizes.width / sizes.height, 
@@ -28,6 +55,11 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.2);
+scene.add(ambientLight);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2.0);
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
@@ -44,7 +76,8 @@ window.addEventListener('resize', () => {
   camera.aspect = sizes.width / sizes.height;
   camera.updateProjectionMatrix();
 
-  //update renderer  renderer.setSize(sizes.width, sizes.height);
+  //update renderer
+  renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
